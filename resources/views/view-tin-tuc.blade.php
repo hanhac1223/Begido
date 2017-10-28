51ae4ce8c5da7a30ad64d99a203cd1b189cc7928
hanhac1223 <hanhac1223@users.noreply.github.com>
2017-10-28 12:31:13 +0700

    bbb

--- resources/views/view-tin-tuc.blade.php
+++ resources/views/view-tin-tuc.blade.php
@@ -2,11 +2,135 @@
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset("css/bootstrap.min.css") }}"/>
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset("css/themify-icons.css") }}"/>
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset("css/custom.css") }}"/>
-<link rel="stylesheet" type="text/css" media="all" href="{{ asset("css/style.css") }}"/>
+
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset("css/responsive.css") }}"/>
 <link rel="stylesheet" type="text/css" media="all" href="{{ asset("css/owl.carousel.min.css") }}"/>
 <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css">
 <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+<style>
+    .tag{
+        padding: .3rem 1rem;
+        margin: .3rem .5rem;
+        white-space: nowrap;
+        border-radius: .8rem;
+        background: #f2f2f2;
+        color: inherit;
+        font-size: 14px;
+        display: inline-table;
+    }
+    .cang-le {
+        padding: 5px 70px 5px 70px;
+    }
+
+    h1 {
+        font-size: 22px;
+    }
+
+    h2 {
+        font-size: 20px;
+    }
+
+    h3 {
+        font-size: 18px;
+    }
+
+    h4 {
+        font-size: 16px;
+    }
+
+    h5 {
+        font-size: 14px;
+    }
+
+    h6 {
+        font-size: 12px;
+    }
+
+    p {
+        font-size: 13px;
+    }
+
+    h1, h2, h3, h4, h5, h6, p {
+        word-wrap: break-word;
+        line-height: 26px;
+        margin-bottom: 10px;
+    }
+
+    .anh-cover {
+        padding-bottom: 1rem;
+        height: 260px;
+        width: 349.984px;
+        background: no-repeat 50% 50%/cover;
+        display: block;
+    }
+
+    .nen-tieu-de-tin-tuc {
+        padding: 2rem;
+        color: #fff;
+        background: linear-gradient(0deg, #000 0, transparent 100%);
+    }
+
+    .khung-tin-tuc {
+        width: 393px;
+        height: 210.344px;
+    }
+
+    .chu-tieu-de-tin-tuc {
+        position: absolute;
+        left: 15px;
+        right: 15px;
+        top: 5px;
+        bottom: 0;
+    }
+
+    .padding-all-x1 {
+        padding: 1rem;
+    }
+
+    .width-100 {
+        width: 100%;
+    }
+
+    .bottom-x0 {
+        bottom: 0;
+    }
+
+    .absolute {
+        position: absolute;
+    }
+
+    .bg-text-gradiant {
+        color: #FFFFFF;
+        background: linear-gradient(0deg, #000 0, transparent 100%);
+    }
+
+
+    .h3, h3 {
+        font-size: 1.4rem;
+    }
+
+    .tac-gia-tin-tuc {
+        padding-bottom: 1rem;
+        display: block;
+        color: #666;
+    }
+
+    .gioi-thieu-tin-tuc {
+        padding-bottom: 1rem;
+        color: #333;
+    }
+
+    .list-danh-muc {
+        list-style-type: none;
+    }
+    .ten-tieu-de-tin-tuc{
+        line-height: 22px;
+    }
+    .khung-tieu-de{
+        height: 84px;
+    }
+
+</style>
 <div class="container-fluid">
     <div class="row cang-le">
         <nav aria-label="breadcrumb" role="navigation">
@@ -31,8 +155,8 @@
 
                     </div>
                     <div class="col-12">
-                    <span class="absolute bottom-x0 padding-all-x1 width-100 bg-text-gradiant h3">
-                        <h4><b>ĐẠI GIA VIỆT CHI 14 TỶ ĐỒNG SỞ HỮU ĐỒNG HỒ NẠM KIM CƯƠNG</b></h4>
+                    <span class="absolute bottom-x0 padding-all-x1 width-100 bg-text-gradiant h3 khung-tieu-de">
+                        <h4 class="ten-tieu-de-tin-tuc"><b>ĐẠI GIA VIỆT CHI 14 TỶ ĐỒNG SỞ HỮU ĐỒNG HỒ NẠM KIM CƯƠNG</b></h4>
                     </span>
                     </div>
                 </a>
@@ -71,8 +195,8 @@
                              alt="" class="khung-tin-tuc">
                     </div>
                     <div class="col-12">
-                    <span class="absolute bottom-x0 padding-all-x1 width-100 bg-text-gradiant h3">
-                        <h4><b>ĐẠI GIA VIỆT CHI 14 TỶ ĐỒNG SỞ HỮU ĐỒNG HỒ NẠM KIM CƯƠNG</b></h4>
+                    <span class="absolute bottom-x0 padding-all-x1 width-100 bg-text-gradiant h3 khung-tieu-de">
+                        <h4 class="ten-tieu-de-tin-tuc"><b>ĐẠI GIA VIỆT CHI 14 TỶ ĐỒNG SỞ HỮU ĐỒNG HỒ NẠM KIM CƯƠNG</b></h4>
                     </span>
                     </div>
                 </a>
@@ -111,8 +235,8 @@
                              alt="" class="khung-tin-tuc">
                     </div>
                     <div class="col-12">
-                    <span class="absolute bottom-x0 padding-all-x1 width-100 bg-text-gradiant h3">
-                        <h4><b>ĐẠI GIA VIỆT CHI 14 TỶ ĐỒNG SỞ HỮU ĐỒNG HỒ NẠM KIM CƯƠNG</b></h4>
+                    <span class="absolute bottom-x0 padding-all-x1 width-100 bg-text-gradiant h3 khung-tieu-de">
+                        <h4 class="ten-tieu-de-tin-tuc"><b>ĐẠI GIA VIỆT CHI 14 TỶ ĐỒNG SỞ HỮU ĐỒNG HỒ NẠM KIM CƯƠNG</b></h4>
                     </span>
                     </div>
                 </a>
