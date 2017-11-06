67697947def85f0adc419987a2cf6c68f93cd5de
Mars <ng.h.anhphuong@github.com>
2017-11-06 21:02:49 +0700

    .

--- resources/views/danh-muc-chi-tiet/danh-muc-chi-tiet.blade.php
+++ resources/views/danh-muc-chi-tiet/danh-muc-chi-tiet.blade.php
@@ -10,7 +10,7 @@
 </div>
 <div class="row danh-sach-tin-tuc">
     <div class="row mot-khoi">
-        <div class="col-4">
+        <div class="col-sm-6 col-md-4">
             <div class="tren">
                 <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
                 <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
@@ -24,7 +24,7 @@
                 </div>
             </div>
         </div> {{--END col-4--}}
-        <div class="col-4">
+        <div class="col-sm-6 col-md-4">
             <div class="tren">
                 <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
                 <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
@@ -38,51 +38,7 @@
                 </div>
             </div>
         </div> {{--END col-4--}}
-        <div class="col-4">
-            <div class="tren">
-                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
-                <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
-            </div>
-            <div class="duoi">
-                <div class="thong-tin">
-                    6/11/2017 | <a href="">Phương</a>
-                </div>
-                <div class="noi-dung-chi-tiet">
-                    <a href="">Một bên cảm phục Chris với lòng dũng cảm và bốc đồng của tuổi trẻ, đã dám đốt cháy tất cả để một lần được sống như mình muốn. Một bên quay lưng với chủ nghĩa lãng mạn và gọi quyết định của Chris là ngông cuồng, và cái chết của anh là vô nghĩa.</a>
-                </div>
-            </div>
-        </div> {{--END col-4--}}
-    </div> {{--END: row mot-khoi--}}
-    <div class="row mot-khoi">
-        <div class="col-4">
-            <div class="tren">
-                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
-                <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
-            </div>
-            <div class="duoi">
-                <div class="thong-tin">
-                    6/11/2017 | <a href="">Phương</a>
-                </div>
-                <div class="noi-dung-chi-tiet">
-                    <a href="">Một bên cảm phục Chris với lòng dũng cảm và bốc đồng của tuổi trẻ, đã dám đốt cháy tất cả để một lần được sống như mình muốn. Một bên quay lưng với chủ nghĩa lãng mạn và gọi quyết định của Chris là ngông cuồng, và cái chết của anh là vô nghĩa.</a>
-                </div>
-            </div>
-        </div> {{--END col-4--}}
-        <div class="col-4">
-            <div class="tren">
-                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
-                <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
-            </div>
-            <div class="duoi">
-                <div class="thong-tin">
-                    6/11/2017 | <a href="">Phương</a>
-                </div>
-                <div class="noi-dung-chi-tiet">
-                    <a href="">Một bên cảm phục Chris với lòng dũng cảm và bốc đồng của tuổi trẻ, đã dám đốt cháy tất cả để một lần được sống như mình muốn. Một bên quay lưng với chủ nghĩa lãng mạn và gọi quyết định của Chris là ngông cuồng, và cái chết của anh là vô nghĩa.</a>
-                </div>
-            </div>
-        </div> {{--END col-4--}}
-        <div class="col-4">
+        <div class="col-sm-6 col-md-4">
             <div class="tren">
                 <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
                 <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
@@ -98,7 +54,7 @@
         </div> {{--END col-4--}}
     </div> {{--END: row mot-khoi--}}
     <div class="row mot-khoi">
-        <div class="col-4">
+        <div class="col-sm-6 col-md-4">
             <div class="tren">
                 <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
                 <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
@@ -112,7 +68,7 @@
                 </div>
             </div>
         </div> {{--END col-4--}}
-        <div class="col-4">
+        <div class="col-sm-6 col-md-4">
             <div class="tren">
                 <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
                 <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
@@ -126,7 +82,51 @@
                 </div>
             </div>
         </div> {{--END col-4--}}
-        <div class="col-4">
+        <div class="col-sm-6 col-md-4">
+            <div class="tren">
+                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
+                <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
+            </div>
+            <div class="duoi">
+                <div class="thong-tin">
+                    6/11/2017 | <a href="">Phương</a>
+                </div>
+                <div class="noi-dung-chi-tiet">
+                    <a href="">Một bên cảm phục Chris với lòng dũng cảm và bốc đồng của tuổi trẻ, đã dám đốt cháy tất cả để một lần được sống như mình muốn. Một bên quay lưng với chủ nghĩa lãng mạn và gọi quyết định của Chris là ngông cuồng, và cái chết của anh là vô nghĩa.</a>
+                </div>
+            </div>
+        </div> {{--END col-4--}}
+    </div> {{--END: row mot-khoi--}}
+    <div class="row mot-khoi">
+        <div class="col-sm-6 col-md-4">
+            <div class="tren">
+                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
+                <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
+            </div>
+            <div class="duoi">
+                <div class="thong-tin">
+                    6/11/2017 | <a href="">Phương</a>
+                </div>
+                <div class="noi-dung-chi-tiet">
+                    <a href="">Một bên cảm phục Chris với lòng dũng cảm và bốc đồng của tuổi trẻ, đã dám đốt cháy tất cả để một lần được sống như mình muốn. Một bên quay lưng với chủ nghĩa lãng mạn và gọi quyết định của Chris là ngông cuồng, và cái chết của anh là vô nghĩa.</a>
+                </div>
+            </div>
+        </div> {{--END col-4--}}
+        <div class="col-sm-6 col-md-4">
+            <div class="tren">
+                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
+                <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
+            </div>
+            <div class="duoi">
+                <div class="thong-tin">
+                    6/11/2017 | <a href="">Phương</a>
+                </div>
+                <div class="noi-dung-chi-tiet">
+                    <a href="">Một bên cảm phục Chris với lòng dũng cảm và bốc đồng của tuổi trẻ, đã dám đốt cháy tất cả để một lần được sống như mình muốn. Một bên quay lưng với chủ nghĩa lãng mạn và gọi quyết định của Chris là ngông cuồng, và cái chết của anh là vô nghĩa.</a>
+                </div>
+            </div>
+        </div> {{--END col-4--}}
+        <div class="col-sm-6 col-md-4">
             <div class="tren">
                 <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
                 <div class="tieu-de">Chris McCandless: Vứt bỏ vật chất để lang thang vào miền hoang dã, và cái chết gây tranh cãi suốt hàng chục năm</div>
@@ -142,5 +142,24 @@
         </div> {{--END col-4--}}
     </div> {{--END: row mot-khoi--}}
 </div> {{--END: danh-sach-tin-tuc--}}
-
+{{--PHÂN TRANG--}}
+<nav aria-label="Page navigation example">
+    <ul class="pagination">
+        <li class="page-item">
+            <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                <span class="sr-only">Previous</span>
+            </a>
+        </li>
+        <li class="page-item"><a class="page-link" href="#">1</a></li>
+        <li class="page-item"><a class="page-link" href="#">2</a></li>
+        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+            <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                <span class="sr-only">Next</span>
+            </a>
+        </li>
+    </ul>
+</nav>
 @endsection
