3fe49edad5243e1a01078a8e09718b85ea9219d4
Mars <ng.h.anhphuong@github.com>
2017-11-06 23:06:29 +0700

    .

--- resources/views/danh-muc-chi-tiet/danh-muc-chi-tiet.blade.php
+++ resources/views/danh-muc-chi-tiet/danh-muc-chi-tiet.blade.php
@@ -25,8 +25,13 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-12">
-        <h2><a href="#">SON</a></h2>
+    <div class="col-12 khoi-tieu-de-lon">
+        <div class="row tieu-de-lon">
+            <h1><a href="#">SON</a></h1>
+        </div>
+        <div class="row noi-dung-danh-muc">
+            14 năm trôi qua, kể từ một ngày tháng 9 năm 1992, thân xác hoang tàn của Christopher McCardless đã được tìm thấy bởi một thợ săn, ngay ngoài rìa của vườn Quốc gia và Bảo tồn Denali, Alaska, Mỹ. Christopher đã chết được 19 ngày tính từ lúc được tìm ra, với cân nặng chỉ còn 30kg. Chris đã sống trong một chiếc xe bus bỏ hoang ở đây, đã săn bắn để sinh tồn, và, viết cả nhật ký nữa. Nguyên nhân cái chết được xác định là chết đói. Christopher đã chống chọi những ngày cuối cùng của đời mình khi không thể săn được thức ăn, và phải ăn củ quả dại để tồn tại.
+        </div>
     </div>
 </div>
 <div class="row danh-sach-tin-tuc">
@@ -163,5 +168,37 @@
         </div> {{--END mot-tin --}}
     </div> {{--END: row mot-khoi--}}
 </div> {{--END: danh-sach-tin-tuc--}}
+
+<div class="danh-muc-lien-quan">
+    <div class="row nhom-tin-lien-quan">
+        <div class="col-sm-4 mot-tin">
+            <h2><a href="">Đồng hồ CASIO</a></h2>
+            <div class="col-12 hinh-anh">
+                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
+            </div>
+            <div class="col-12 noi-dung">
+                Quay lại thời gian trước đó, vào năm 1990, Christopher - khi đó là một sinh viên đã tốt nghiệp. Có 47 nghìn đô la trong tay, Chris quyên góp 24 nghìn đô la vào một tổ chức từ thiện và quyết định từ bỏ tất cả cuộc sống hiện tại, dấn thân vào một cuộc phiêu lưu của cuộc đời mình. Chris đổi tên thành Alexander Supertramp (Alexander kẻ siêu lang thang), tiến về những miền đất mới và làm việc như một người lao động chân tay. Chris sống chuỗi ngày của một kẻ lang thang đích thực, khi có tiền, khi không, tránh tiếp xúc với mọi người. Anh hạnh phúc khi sống với những tiếp xúc loài người, đồ đạc và nhu yếu phẩm một cách tối thiểu - như một người hoang dã. Cho đến tháng 4 năm 1992, Chris quyết định đến Alaska với tình trạng không có một vật dụng phù hợp và kinh nghiệm để sống ở một miền hoang dã. Thậm chí, Chris còn không có một chiếc la bàn.
+            </div>
+        </div> {{--END mot-tin--}}
+        <div class="col-sm-4 mot-tin">
+            <h2><a href="">Đồng hồ CASIO</a></h2>
+            <div class="col-12 hinh-anh">
+                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
+            </div>
+            <div class="col-12 noi-dung">
+                Quay lại thời gian trước đó, vào năm 1990, Christopher - khi đó là một sinh viên đã tốt nghiệp. Có 47 nghìn đô la trong tay, Chris quyên góp 24 nghìn đô la vào một tổ chức từ thiện và quyết định từ bỏ tất cả cuộc sống hiện tại, dấn thân vào một cuộc phiêu lưu của cuộc đời mình. Chris đổi tên thành Alexander Supertramp (Alexander kẻ siêu lang thang), tiến về những miền đất mới và làm việc như một người lao động chân tay. Chris sống chuỗi ngày của một kẻ lang thang đích thực, khi có tiền, khi không, tránh tiếp xúc với mọi người. Anh hạnh phúc khi sống với những tiếp xúc loài người, đồ đạc và nhu yếu phẩm một cách tối thiểu - như một người hoang dã. Cho đến tháng 4 năm 1992, Chris quyết định đến Alaska với tình trạng không có một vật dụng phù hợp và kinh nghiệm để sống ở một miền hoang dã. Thậm chí, Chris còn không có một chiếc la bàn.
+            </div>
+        </div> {{--END mot-tin--}}
+        <div class="col-sm-4 mot-tin">
+            <h2><a href="">Đồng hồ CASIO</a></h2>
+            <div class="col-12 hinh-anh">
+                <img src="https://images.moviepilot.com/image/upload/c_fill,h_630,q_auto:best,w_1200/into-the-wild-a-love-confession-to-my-favorite-film-jpeg-282684.jpg" alt="Hình ảnh">
+            </div>
+            <div class="col-12 noi-dung">
+                Quay lại thời gian trước đó, vào năm 1990, Christopher - khi đó là một sinh viên đã tốt nghiệp. Có 47 nghìn đô la trong tay, Chris quyên góp 24 nghìn đô la vào một tổ chức từ thiện và quyết định từ bỏ tất cả cuộc sống hiện tại, dấn thân vào một cuộc phiêu lưu của cuộc đời mình. Chris đổi tên thành Alexander Supertramp (Alexander kẻ siêu lang thang), tiến về những miền đất mới và làm việc như một người lao động chân tay. Chris sống chuỗi ngày của một kẻ lang thang đích thực, khi có tiền, khi không, tránh tiếp xúc với mọi người. Anh hạnh phúc khi sống với những tiếp xúc loài người, đồ đạc và nhu yếu phẩm một cách tối thiểu - như một người hoang dã. Cho đến tháng 4 năm 1992, Chris quyết định đến Alaska với tình trạng không có một vật dụng phù hợp và kinh nghiệm để sống ở một miền hoang dã. Thậm chí, Chris còn không có một chiếc la bàn.
+            </div>
+        </div> {{--END mot-tin--}}
+    </div>
+</div>
 
 @endsection
